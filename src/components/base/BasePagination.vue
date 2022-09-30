d88cbc120b2d3d25eddd8b53c7ec5b72e2860642
cong <loitran299@gmail.com>
2022-09-30 08:09:30 +0700

    update

---
+++ src/components/base/BasePagination.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pagination">
+    sdfdsfsd
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import url('@/assets/css/base/pagination.css');
+</style>
